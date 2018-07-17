3971a6de5b80682b66e4144f3850033565fb64bc
eMe-404 <838702939>
2018-07-17 14:27:48 +0800

    [yedili] - oo add class Dog and Fish

--- specs/oo_practise1.js
+++ specs/oo_practise1.js
@@ -11,7 +11,29 @@ class Animal {
     }
 }
 
+class Dog extends Animal{
+    constructor(name, color) {
+        super(name, 4, color);
+    }
+}
+
+class Fish extends Animal {
+    constructor(name, color) {
+        super(name, 0, color);
+    }
+
+    describe() {
+        console.log("I know how to swim! And my name is :" + this.name);
+    }
+}
+
 const cc = new Animal("cc", 4, "black");
 const dd = new Animal("dd", 2, "red");
 cc.describe();
 dd.describe();
+
+const dog1 = new Dog("doggy", "yellow");
+dog1.describe();
+
+const fish1 = new Fish("LL", "white");
+fish1.describe();
